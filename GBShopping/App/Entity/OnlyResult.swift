9c3c0538f544b2e2ce9772fd827b2f71df82ba26
gregorian.st <gregorian.st@gmail.com>
2021-06-24 16:50:09 +0300

    Auth functionality has been added

---
+++ GBShopping/App/Entity/OnlyResult.swift
@@ -0,0 +1,12 @@
+//
+//  LogoutResult.swift
+//  GBShopping
+//
+//  Created by Grigory Stolyarov on 24.06.2021.
+//
+
+import Foundation
+
+struct LogoutResult: Codable {
+    let result: Int
+}
---
+++ GBShopping/App/Entity/RegisterUserResult.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterUserResult.swift
+//  GBShopping
+//
+//  Created by Grigory Stolyarov on 24.06.2021.
+//
+
+import Foundation
